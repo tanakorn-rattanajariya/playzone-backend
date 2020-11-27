1613fb603dd6060ad3900fb5c520d3ce30dc25d0
Tanakorn Rattanajariya <lufas2603@gmail.com>
2020-11-28 00:59:20 +0700

    init project

---
+++ src/main/java/com/snowball/playzone/PlayzoneApplication.java
@@ -0,0 +1,13 @@
+package com.snowball.playzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowball/playzone/PlayzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowball.playzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
